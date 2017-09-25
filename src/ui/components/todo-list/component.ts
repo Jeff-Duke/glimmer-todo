6ef730b211ebef107f0d78ad98aa27010862e31d
Jeff Duke <Jeff.Duke@Gmail.com>
2017-09-25 11:49:51 -0600

    push up working app (#1)

--- src/ui/components/todo-list/component.ts
+++ src/ui/components/todo-list/component.ts
@@ -1,5 +1,61 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class TodoList extends Component {
+  @tracked newItemText = '';
 
+  @tracked items = [
+    {
+      text: 'install glimmer',
+      isDone: false
+    },
+    {
+      text: 'build a nice todo list',
+      isDone: false
+    },
+    {
+      text: 'celebrate!!',
+      isDone: false
+    }
+  ];
+
+  updateNewItemText(e) {
+    this.newItemText = e.target.value;
+  }
+
+  addItem() {
+    if (!this.newItemText) return false;
+
+    let items = [
+      ...this.items,
+      {
+        text: this.newItemText,
+        isDone: false
+      },
+    ];
+
+    this.items = items;
+    this.newItemText = '';
+  }
+
+  deleteItem(removedItem) {
+    let items = this.items.filter(item => {
+      return item.text !== removedItem.text;
+    });
+
+    this.items = items;
+  }
+
+  toggleItem(toggledItem) {
+    let items = this.items.map(item => {
+      if (item.text === toggledItem.text) {
+        return Object.assign(item, {
+          isDone: !item.isDone,
+        });
+      } else {
+        return item;
+      }
+    });
+
+    this.items = items;
+  }
 }
